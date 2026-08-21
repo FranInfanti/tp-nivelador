6e4407a07143ee2f4323028f14dbb5aa7f479d1b
FranInfanti <francisco.infanti@gmail.com>
2026-08-21 17:39:00 +0000

    feat: allows the server to save the files uploaded by clients

--- services/server/src/server/server.py
+++ services/server/src/server/server.py
@@ -1,14 +1,21 @@
+import os
+
 import socket
 import logger
 import safe_socket
 
 _ECHO_SERVER_MESSAGE_SIZE = 1024
 
+OUTPUT_FILE = os.environ["OUTPUT_FILE"]
 
 class Server:
     def __init__(self, server_host: str, server_port: int) -> None:
         self.server_host = server_host
         self.server_port = server_port
+
+    def _save_message(self, client_message):
+        with open(OUTPUT_FILE, "a", encoding="utf-8") as file:
+            file.write(f"{client_message.decode('utf-8')}\n")
 
     def _handle_client(self, client_socket):
         action = "handle-client"
@@ -28,6 +35,9 @@ class Server:
                     )
                     return
                 message_amount += 1
+
+                self._save_message(client_message)
+
                 safe_socket.send_all(client_socket, client_message)
         except Exception as e:
             logger.error(
