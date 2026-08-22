e620bf1ce03b2c6b8c993f48e1d4e0dadaa71253
FranInfanti <francisco.infanti@gmail.com>
2026-08-22 16:36:38 +0000

    feat: implements solutions to the problem of short-read and short-write

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -2,21 +2,33 @@ package safe_socket
 
 import "io"
 
-//TODO: Complete with a short-read/short-write tolerant implementation
-
 func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
-	if err != nil {
-		return err
+	bytesWritten := 0
+	for bytesWritten < len(bytes) {
+		n, err := socket.Write(bytes[bytesWritten:])
+		if err != nil {
+			return err
+		}
+
+		bytesWritten += n
 	}
+
 	return nil
 }
 
 func RecvAll(socket io.Reader, size int) ([]byte, error) {
 	buff := make([]byte, size)
-	_, err := socket.Read(buff)
-	if err != nil {
-		return nil, err
-	}
+	
+	bytesRead := 0
+	for bytesRead < size {
+		n, err := socket.Read(buff[bytesRead:])
+
+		if err != nil {
+			return buff, err
+		}
+
+		bytesRead += n
+	}	
+
 	return buff, nil
 }
--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -1,11 +1,24 @@
 import socket
 
-# TODO: Complete with a short-read/short-write tolerant implementation
-
-
 def recv_all(socket: socket.socket, size):
-    return socket.recv(size)
+    buff = b""
+    
+    bytesRemaining = size
+    while len(buff) < size:
+        try: 
+            buff += socket.recv(bytesRemaining)
+            bytesRemaining = size - len(buff)
+        except OSError as err:
+            return buff, err
 
+    return buff, None
 
 def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+    bytesWritten = 0
+
+    while bytesWritten < len(bytes):
+        try:
+            n = socket.send(bytes[bytesWritten:])
+            bytesWritten += n
+        except OSError as err:
+            return err
